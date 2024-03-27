8be8ed4b5bc3eb58e721bf4e0d95560db9308f48
Roberto <robertoboero@outlook.it>
2024-03-27 22:43:48 +0100

    120. Dynamic & Conditional Styling with Tailwind

--- 06-styling-project/src/components/AuthInputs.jsx
+++ 06-styling-project/src/components/AuthInputs.jsx
@@ -1,4 +1,14 @@
 import { useState } from "react";
+import { styled } from "styled-components";
+import Button from "./Button.jsx";
+import Input from "./Input";
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -22,36 +32,36 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
-        <p>
-          <label>Email</label>
-          <input
-            type="email"
-            //className={emailNotValid ? 'invalid' : undefined}
-            style={{
+      <ControlContainer>
+        <Input
+          invalid={emailNotValid}
+          type="email"
+          onChange={(event) => handleInputChange("email", event.target.value)}
+          /* className={`label ${emailNotValid ? "invalid" : undefined}`} */
+          /*  style={{
               backgroundColor: emailNotValid ? "red" : "#fff",
-            }}
-            onChange={(event) => handleInputChange("email", event.target.value)}
-          />
-        </p>
-        <p>
-          <label>Password</label>
-          <input
-            type="password"
-            className={passwordNotValid ? "invalid" : undefined}
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-          />
-        </p>
-      </div>
+            }} */
+        >
+          Email
+        </Input>
+        <Input
+          invalid={passwordNotValid}
+          type="password"
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+          /* className={passwordNotValid ? "invalid" : undefined} */
+        >
+          Password
+        </Input>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <Button className="button" onClick={handleLogin}>
           Sign In
-        </button>
+        </Button>
       </div>
     </div>
   );
