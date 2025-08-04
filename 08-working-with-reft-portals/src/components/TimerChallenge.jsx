6fedd4ef5890d6ff9e406119d04a8a6d60ff921c
Roberto Boero <robertoboero@MacBook-Air-di-Roberto.local>
2025-08-04 15:25:10 +0200

    Prima versione (import da locale)

---
+++ 08-working-with-reft-portals/src/components/TimerChallenge.jsx
@@ -0,0 +1,45 @@
+import { useRef, useState } from "react";
+import ResultModal from "./ResultModal";
+
+export default function TimerChallange({ title, targetTime }) {
+  const timer = useRef();
+  const dialog = useRef();
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timer.current =  setTimeout(() => {
+      setTimerExpired(true),
+      dialog.current.showModal();
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime}  result='lost' />
+
+    <section className="challenge">
+      <h2>{title}</h2>
+      
+      <p className="challenge-time">
+        {targetTime}
+        second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"}
+        </button>
+      </p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Timer is running" : "Timer inactive"}
+      </p>
+    </section>
+    </>
+  );
+}
